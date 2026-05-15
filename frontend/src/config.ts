635015a8f9967f5db0c6c4a12a77da0bf7f7e931
Jitesh117 <sahookumarjitesh@gmail.com>
2026-05-15 13:46:53 +0530

    fix: frontend config change

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,5 @@
-export const WS_URL = 'ws://localhost:8080/ws'
-export const API_URL = 'http://localhost:8080'
+export const API_URL = import.meta.env.VITE_API_URL
+export const WS_URL = import.meta.env.VITE_WS_URL
 
 export const PLAYER_ID_SESSION_KEY = 'vimRoyalePlayerIdSession'
 export const PLAYER_ID_STORAGE_KEY = 'vimRoyalePlayerId'
